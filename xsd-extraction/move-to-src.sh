a30e66b42316caf6bf83f4bfd3eb7f4cc3ec2837
Ben Oesch <ben.oesch@hs-esslingen.de>
2020-10-01 14:50:45 +0200

    Added test code for checking JDK version. Added move script.

---
+++ xsd-extraction/move-to-src.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -r ../src/main/java/eu
+rm -r ../src/main/java/uk
+rm -r ../src/main/java/de/vdv
+
+mv ./java/eu ../src/main/java/
+mv ./java/uk ../src/main/java/
+mv ./java/de/vdv ../src/main/java/de/
+
+rm -r ./java
--- xsd-extraction/regenerate-java-classes.sh
+++ xsd-extraction/regenerate-java-classes.sh
@@ -2,10 +2,27 @@
 
 TARGET_DIR=../java
 
+if [[ -z "$1" ]]; then
+  JDK8="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+else
+  JDK8="$1"
+fi
+
+if [[ ! -d "$JDK8" ]]; then
+    echo "Unable to locate JDK 1.8 (looking at $JDK8). The xjc command must be run with this version to ensure correct results. Please install this JDK version on you system and/or specify he location of it as first argument to this script."
+    exit 1
+fi
+
+XJC="$JDK8/bin/xjc"
+echo "Using $XJC for class generation."
+
 # If trias-xsd dir does not exist yet, extract the package...
 if [[ ! -d trias-xsd ]]; then
+    echo "Unzipping trias schema files..."
     unzip trias-xsd-v1.2 -d trias-xsd
 fi
+
+echo "Preparing java code generation..."
 
 # Switch to directory and make
 cd trias-xsd
@@ -21,12 +38,14 @@ mkdir "$TARGET_DIR"
 # Overwrite bindings file with current one
 cp ../bindings.xml ./bindings.xml
 
+echo "Generating classes..."
 # Compile classes
-xjc -d "$TARGET_DIR" -npa -extension -classpath ../../src/main/java/ -b bindings.xml Trias.xsd
+"$XJC" -d "$TARGET_DIR" -npa -extension -classpath ../../src/main/java/ -b bindings.xml Trias.xsd > /dev/null
 
 # Remove local copy of bindings.xml to avoid confusion with original file.
 rm bindings.xml
 
+echo "Starting post processing script..."
 # Run post processing...
 ../post-process.sh $TARGET_DIR
 
