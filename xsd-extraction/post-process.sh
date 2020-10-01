a64fa772540d1110bcbb3d47f950952aa37d5040
Ben Oesch <ben.oesch@hs-esslingen.de>
2020-10-01 14:23:27 +0200

    Worked on post-processing of files. Refactoring file names for ease of use.

--- xsd-extraction/post-process.sh
+++ xsd-extraction/post-process.sh
@@ -30,6 +30,10 @@ fi
 # Get directory where this script resides.
 SDIR="$(dirname "$(readlink -f "$0")")"
 
+CLASSPAT="[a-zA-Z_0-9\.]+" # Classname Pattern
+SUFFIXPAT="Structure" # Suffix Pattern To Remove
+EOLPAT="([ {]*)\\\$?"
+
 processFile(){
     srcfile=$1
     licensefile=$SDIR/licenseheader.txt
@@ -44,6 +48,20 @@ processFile(){
     # 4. Overwrite the source code file with the new content.
 
     cat "$licensefile" "$srcfile" | sed -e "0,/yyyy/ {s/yyyy/$YEAR/}" -e '\_// Generated on: [0-9\.]* at [0-9:]* .*$_ d' | sponge "$srcfile"
+    sed -i -E -e "s/import ($CLASSPAT)$SUFFIXPAT/import \1/" \
+        -e "s/public class ($CLASSPAT)$SUFFIXPAT$EOLPAT/public class \1\2/" \
+        -e "s/public abstract class ($CLASSPAT)$SUFFIXPAT$EOLPAT/public abstract class \1\2/" \
+        -e "s/public interface ($CLASSPAT)$SUFFIXPAT$EOLPAT/public interface \1\2/" \
+        -e "s/ extends ($CLASSPAT)$SUFFIXPAT$EOLPAT/ extends \1\2/" \
+        -e "s/ implements ($CLASSPAT)$SUFFIXPAT$EOLPAT/ implements \1\2/" \
+        -e "s/([ \(<])($CLASSPAT)$SUFFIXPAT([ \(\)>\.])/\1\2\3/g" \
+        "$srcfile"
+
+    if [[ "$srcfile" =~ "$SUFFIXPAT.java" ]]; then
+        local NEWNAME=$(echo "$srcfile" | sed -e "s/$SUFFIXPAT\.java/.java/")
+        # log "[$YELLOW Renaming $NONE]: $RED$(basename $srcfile)$NONE to $GREEN$(basename $NEWNAME)$NONE"
+        mv "$srcfile" "$NEWNAME"
+    fi
 }
 
 
@@ -55,7 +73,6 @@ find "$TARGET_DIR" -name '*.java' -print0 |
         processFile "$line"
     done
 
-
 # Make replacements in various files
 replace(){
     log "[$YELLOW Replacing $NONE]: \"$RED$2$NONE\" with \"$GREEN$3$NONE\" in file $PURPLE$1$NONE"
@@ -64,13 +81,19 @@ replace(){
 
 # Some reusable variables for convenience:
 VDV="$TARGET_DIR/de/vdv/trias"
+SIRI="$TARGET_DIR/uk/org/siri/siri"
 PACKAGE="de.hsesslingen.keim.efs.adapter.trias.supertypes"
 implements="implements $PACKAGE."
 
-replace "$VDV/LegAlightStructure.java" "class LegAlightStructure" "class LegAlightStructure $implements ILegEnd"
-replace "$VDV/LegBoardStructure.java" "class LegBoardStructure" "class LegBoardStructure $implements ILegEnd"
+replace "$VDV/LegAlight.java" "class LegAlight" "class LegAlight $implements ILegEnd"
+replace "$VDV/LegBoard.java" "class LegBoard" "class LegBoard $implements ILegEnd"
 
-# Implementing interface ICoordinates on GeoPositionStructure. (Adding interface and methods)
-replace "$VDV/GeoPositionStructure.java" "class GeoPositionStructure" "class GeoPositionStructure implements de.hsesslingen.keim.efs.middleware.model.ICoordinates"
-replace "$VDV/GeoPositionStructure.java" "^}$" "\t@Override\n\tpublic Double getLat() {\n\t\treturn latitude;\n\t}\n\n\t@Override\n\tpublic Double getLon() {\n\t\treturn longitude;\n\t}\n}"
+# Implementing interface ICoordinates on GeoPosition. (Adding interface and methods)
+replace "$VDV/GeoPosition.java" "class GeoPosition" "class GeoPosition implements de.hsesslingen.keim.efs.middleware.model.ICoordinates"
+replace "$VDV/GeoPosition.java" "^}$" "\t@Override\n\tpublic Double getLat() {\n\t\treturn latitude;\n\t}\n\n\t@Override\n\tpublic Double getLon() {\n\t\treturn longitude;\n\t}\n}"
+
+# Fixing some errors due to refactoring in processFile function
+replace "$SIRI/AffectsScope.java" "extends AffectedNetwork" "extends uk.org.siri.siri.AffectedNetwork"
+replace "$SIRI/PtSituationElement.java" "extends Image" "extends uk.org.siri.siri.Image"
+replace "$SIRI/VehicleActivity.java" "extends MonitoredVehicleJourney" "extends uk.org.siri.siri.MonitoredVehicleJourney"
 
